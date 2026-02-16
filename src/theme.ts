5ba3aa02237b53dc1e9b491b70c0d730dc41c604
Kauan Farias <arrudakauanfarias@gmail.com>
2026-02-16 19:43:15 -0300

    feat: configs material ui theme and css baseline

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+    secondary: {
+      main: '#4f8e3e',
+    },
+    },
+    typography: {
+        fontFamily: "Segoe UI"
+  }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
