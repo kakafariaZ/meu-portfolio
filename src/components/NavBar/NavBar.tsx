75570ed9c19a039db77e7791bac35d5a47ba074f
Kauan Farias <arrudakauanfarias@gmail.com>
2026-02-17 20:59:12 -0300

    feat: inits NavBar desktop component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-hooks/static-components */
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+
+const NavBar = () => {
+
+    const StyledToobar = styled(Toolbar)(() => ({
+        display: "flex",
+        justifyContent: "space-evenly"
+    }))
+
+    return (
+    <>
+        <AppBar position="absolute">
+            <StyledToobar>
+            <MenuItem>Sobre</MenuItem>
+            <MenuItem>Habilidades</MenuItem>
+            <MenuItem>Projetos</MenuItem>
+            </StyledToobar>
+        </AppBar>
+    </>
+    )
+}
+
+export default NavBar
